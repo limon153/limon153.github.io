d1e165f64a1e08138f373d317df1bd3d6f9c447d
limon153 <limonkeliy153@gmail.com>
2017-08-13 15:17:46 +0300

    Add comments and small fixes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 $(function() {
 
-
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   var firstNumberInput = $('input.first-number-input');
   var secondNumberInput = $('input.second-number-input');
+
+  // Initialize numbers
+  var num1 = Math.floor(Math.random()*4 + 6);
+  var result = Math.floor(Math.random()*4 + 11);
+  var num2 = result - num1;
+
+  // Show numbers
+  $('#first-number').text(num1);
+  $('#second-number').text(num2);
+
+  // Initialize variables for arrows
+  var arrowStartX = 10.5;
+
+  // Control position for quadraticCurveTo function
+  var controlX = 0;
+  var controlY = 0;
+
+  // Length of arrow
+  var path = 0;
+
+  // Check if second arrow already exists
+  var isSecondArrowExists = false;
 
 
   // Draw axis
@@ -37,23 +58,11 @@ $(function() {
   }
 
 
-  var num1 = Math.floor(Math.random()*4 + 6);
-  var result = Math.floor(Math.random()*4 + 11);
-  var num2 = result - num1;
-  $('#first-number').text(num1);
-  $('#second-number').text(num2);
-
-  var arrowStartX = 10.5;
-  var controlX = 0;
-  var controlY = 0;
-  var path = 0;
-  var isSecondArrowExists = false;
+  // Inital drawing of first arrow and positioning of firs number input
   drawArrow(num1);
-  $('.first-number-input').css('top', (controlY) + 'px');
+  $('.first-number-input').removeClass('hidden');
+  $('.first-number-input').css('top', (controlY - 10) + 'px');
   $('.first-number-input').css('left', (controlX - 7) + 'px');
-
-
-
 
 
   function drawArrow(number) {
@@ -74,16 +83,21 @@ $(function() {
     arrowStartX += number * 30;
   }
 
+  // Check first input and create second arrow
   firstNumberInput.on('input keyup', function() {
+
     if (firstNumberInput.val() != num1 && firstNumberInput.val().length != 0) {
-      console.log(firstNumberInput.val());
+      // Wrong number
       firstNumberInput.css('color', 'red');
       $('#first-number').css('background-color', 'orange');
     } else {
+      // Correct number or empty input
       console.log(firstNumberInput.val());
       firstNumberInput.css('color', 'black');
       $('#first-number').css('background-color', 'inherit');
+
       if (!isSecondArrowExists && firstNumberInput.val() == num1) {
+        // Correct number
         $("span.first-number-input").text(firstNumberInput.val());
         firstNumberInput.addClass('hidden');
         secondNumberInput.removeClass('hidden');
@@ -95,14 +109,18 @@ $(function() {
     }
   });
 
+  // Check second input and show input for final result
   secondNumberInput.on('input keyup', function() {
     if (secondNumberInput.val() != num2 && secondNumberInput.val().length != 0) {
+      // Wrong number
       secondNumberInput.css('color', 'red');
       $('#second-number').css('background-color', 'orange');
     } else {
+      // Correct number or empty input
       secondNumberInput.css('color', 'black');
       $('#second-number').css('background-color', 'inherit');
       if (secondNumberInput.val() == num2) {
+        // Correct number
         $("span.second-number-input").text(secondNumberInput.val());
         secondNumberInput.addClass('hidden');
         $('#result-input').removeClass('hidden');
@@ -111,17 +129,24 @@ $(function() {
     }
   });
 
+  // Check final result
   $('#result-form').submit(function(e) {
     if ($('#result-input').val() != result && $('#result-input').val().length != 0) {
+      // Wrong answer
       $('#result-input').css('color', 'red');
-    } else {
-      $('#result-input').css('color', 'black');
-      if ($('#result-input').val() == result) {
-        $("#result").text($('#result-input').val());
-        $('#result-input').addClass('hidden');
-        $('#result').removeClass('hidden');
-      }
+    } else if($('#result-input').val() == result) {
+      // Correct answer
+      $("#result").text($('#result-input').val());
+      $('#result-input').addClass('hidden');
+      $('#result').removeClass('hidden');
     }
     e.preventDefault();
+  });
+
+  // Make text black when input is cleared
+  $('#result-form').on('input keyup', function() {
+    if ($('#result-input').val().length == 0) {
+      $('#result-input').css('color', 'black');
+    }
   });
 });
